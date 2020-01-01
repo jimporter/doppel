ff412708a40da870aafe31a3afba8780b7df8665
Jim Porter <itsjimporter@gmail.com>
2020-01-01 13:49:41 -0800

    Drop support for Python 2

--- setup.py
+++ setup.py
@@ -88,8 +88,6 @@ setup(
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: BSD License',
 
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
